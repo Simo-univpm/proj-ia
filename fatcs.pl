82381b7136f0be141dd973c0066f1cfa30ef45f1
Simo-univpm <s1102917@studenti.univpm.it>
2023-08-29 16:07:44 +0200

    prova nuovo sistema di recommend

--- fatcs.pl
+++ fatcs.pl
@@ -20,7 +20,7 @@ den  è un utente che ha visitato prodotti in maniera mista.
 */
 
 search_history(john, [intel_i9_10900k, amd_ryzen_9_5950x, intel_core_i7_11700k, amd_ryzen_7_5800x, intel_core_i5_11600k]).
-search_history(wick, [amd_ryzen_9_5950x, intel_core_i7_11700k, amd_ryzen_5_5600x, nvidia_geforce_rtx_3080]).
+search_history(wick, [amd_ryzen_9_5950x, intel_core_i7_11700k, nvidia_geforce_rtx_3080]).
 
 search_history(lisa, [nvidia_geforce_rtx_3090, amd_radeon_rx_6900_xt, nvidia_geforce_rtx_3080, amd_radeon_rx_6800_xt, nvidia_geforce_rtx_3070]).
 search_history(carl, [corsair_vengeance_rgb_pro_16gb, g_skill_trident_z_neo_32gb, kingston_hyperx_predator_8gb, crucial_ballistix_32gb, team_group_t_force_delta_16gb]).
@@ -126,60 +126,20 @@ performance(samsung_32gb_ddr4_ecc, 4.4).
 
 % User ratings for products
 user_rating(john, intel_i9_10900k, 4.5).
-user_rating(john, amd_ryzen_9_5950x, 4.8).
-user_rating(john, intel_core_i7_11700k, 4.4).
-user_rating(wick, intel_i9_10900k, 4.5).
-user_rating(wick, amd_ryzen_5_5600x, 4.8).
-user_rating(wick, intel_core_i7_11700k, 4.4).
 user_rating(lisa, nvidia_geforce_rtx_3090, 5.0).
-user_rating(lisa, amd_radeon_rx_6900_xt, 4.6).
-user_rating(lisa, nvidia_geforce_rtx_3080, 4.9).
 user_rating(carl, corsair_vengeance_rgb_pro_16gb, 4.0).
-user_rating(carl, g_skill_trident_z_neo_32gb, 4.2).
-user_rating(carl, kingston_hyperx_predator_8gb, 3.8).
-user_rating(den, intel_i9_10900k, 4.2).
-user_rating(den, nvidia_geforce_rtx_3090, 4.5).
-user_rating(den, corsair_vengeance_rgb_pro_16gb, 4.1).
-user_rating(abb, nvidia_geforce_rtx_3090, 4.7).
-user_rating(abb, intel_i9_10900k, 4.6).
-user_rating(abb, amd_radeon_rx_6900_xt, 4.5).
-user_rating(don, nvidia_geforce_rtx_3090, 4.9).
 user_rating(don, intel_i9_10900k, 4.8).
+user_rating(abb, nvidia_geforce_rtx_3090, 4.7).
+user_rating(den, intel_i9_10900k, 4.2).
+user_rating(john, amd_ryzen_9_5950x, 4.8).
+user_rating(lisa, amd_radeon_rx_6900_xt, 4.6).
+user_rating(carl, g_skill_trident_z_neo_32gb, 4.2).
+user_rating(don, nvidia_geforce_rtx_3090, 4.9).
+user_rating(abb, intel_i9_10900k, 4.6).
+user_rating(den, nvidia_geforce_rtx_3090, 4.5).
+user_rating(john, intel_core_i7_11700k, 4.4).
+user_rating(lisa, nvidia_geforce_rtx_3080, 4.9).
+user_rating(carl, kingston_hyperx_predator_8gb, 3.8).
 user_rating(don, amd_ryzen_7_5800x, 4.7).
-
-% Rule to calculate similarity between two components based on performance
-similarity(Component1, Component2, Similarity) :-
-    performance(Component1, Perf1),
-    performance(Component2, Perf2),
-    Similarity is 1 / (1 + abs(Perf1 - Perf2)).
-
-% Rule to recommend components to a user based on their search history
-recommendations(User, RecommendedComponents) :-
-    search_history(User, SearchHistory),
-    findall(Component, (
-        category(Component, Category),
-        member(Component, SearchHistory),
-        not(user_rating(User, Component, _))
-    ), UnratedComponents),
-    findall(Component-Similarity, (
-        member(Component, UnratedComponents),
-        similarity(Component, PreviousComponent, Similarity),
-        user_rating(User, PreviousComponent, _)
-    ), ComponentSimilarities),
-    sort(2, @>=, ComponentSimilarities, SortedComponentSimilarities),
-    remove_duplicates(SortedComponentSimilarities, UniqueComponentSimilarities),
-    maplist(get_component, UniqueComponentSimilarities, TempRecommendedComponents),
-    remove_duplicates(TempRecommendedComponents, RecommendedComponents).
-
-% Rule to remove duplicates from a list
-remove_duplicates([], []).
-remove_duplicates([X|Xs], [X|Ys]) :-
-    subtract(Xs, [X], Temp),
-    remove_duplicates(Temp, Ys).
-
-% Rule to get the component from a tuple (Component-Similarity)
-get_component(Component-_, Component).
-
-% Sample query to get recommendations for user "john"
-% recommendations(john, RecommendedComponents).
-
+user_rating(abb, amd_radeon_rx_6900_xt, 4.5).
+user_rating(den, corsair_vengeance_rgb_pro_16gb, 4.1).
--- simone_only_backup.pl
+++ simone_only_backup.pl
@@ -2,7 +2,6 @@
 
 % Users
 user(john).
-user(wick).
 user(lisa).
 user(carl).
 user(don).
@@ -20,8 +19,6 @@ den  è un utente che ha visitato prodotti in maniera mista.
 */
 
 search_history(john, [intel_i9_10900k, amd_ryzen_9_5950x, intel_core_i7_11700k, amd_ryzen_7_5800x, intel_core_i5_11600k]).
-search_history(wick, [amd_ryzen_9_5950x, intel_core_i7_11700k, amd_ryzen_5_5600x, nvidia_geforce_rtx_3080]).
-
 search_history(lisa, [nvidia_geforce_rtx_3090, amd_radeon_rx_6900_xt, nvidia_geforce_rtx_3080, amd_radeon_rx_6800_xt, nvidia_geforce_rtx_3070]).
 search_history(carl, [corsair_vengeance_rgb_pro_16gb, g_skill_trident_z_neo_32gb, kingston_hyperx_predator_8gb, crucial_ballistix_32gb, team_group_t_force_delta_16gb]).
 search_history(don, [intel_i9_10900k, nvidia_geforce_rtx_3090, amd_ryzen_9_5950x, intel_core_i7_11700k, nvidia_geforce_rtx_3080]).
@@ -92,94 +89,83 @@ price(a_data_xpg_spectrix_d60g_32gb, 169.99).
 price(mushkin_enhanced_redline_16gb, 119.99).
 price(samsung_32gb_ddr4_ecc, 249.99).
 
-% Performance facts for products
-performance(intel_i9_10900k, 4.7).
-performance(amd_ryzen_9_5950x, 4.9).
-performance(intel_core_i7_11700k, 4.5).
-performance(amd_ryzen_7_5800x, 4.6).
-performance(intel_core_i5_11600k, 4.2).
-performance(amd_ryzen_5_5600x, 4.4).
-performance(intel_pentium_gold_g6600, 3.0).
-performance(amd_athlon_gold_4500g, 3.5).
-performance(intel_celeron_g5905, 2.8).
-performance(amd_ryzen_3_5300g, 3.2).
-performance(nvidia_geforce_rtx_3090, 4.8).
-performance(amd_radeon_rx_6900_xt, 4.7).
-performance(nvidia_geforce_rtx_3080, 4.6).
-performance(amd_radeon_rx_6800_xt, 4.5).
-performance(nvidia_geforce_rtx_3070, 4.3).
-performance(amd_radeon_rx_6700_xt, 4.1).
-performance(nvidia_geforce_rtx_3060_ti, 3.9).
-performance(amd_radeon_rx_6600_xt, 3.8).
-performance(nvidia_geforce_rtx_3050, 3.0).
-performance(amd_radeon_rx_6500_xt, 2.9).
-performance(corsair_vengeance_rgb_pro_16gb, 4.3).
-performance(g_skill_trident_z_neo_32gb, 4.5).
-performance(kingston_hyperx_predator_8gb, 3.6).
-performance(crucial_ballistix_32gb, 4.0).
-performance(team_group_t_force_delta_16gb, 3.8).
-performance(hyperx_fury_rgb_8gb, 3.5).
-performance(patriot_viper_steel_16gb, 3.9).
-performance(a_data_xpg_spectrix_d60g_32gb, 4.2).
-performance(mushkin_enhanced_redline_16gb, 3.7).
-performance(samsung_32gb_ddr4_ecc, 4.4).
-
 % User ratings for products
 user_rating(john, intel_i9_10900k, 4.5).
-user_rating(john, amd_ryzen_9_5950x, 4.8).
-user_rating(john, intel_core_i7_11700k, 4.4).
-user_rating(wick, intel_i9_10900k, 4.5).
-user_rating(wick, amd_ryzen_5_5600x, 4.8).
-user_rating(wick, intel_core_i7_11700k, 4.4).
 user_rating(lisa, nvidia_geforce_rtx_3090, 5.0).
-user_rating(lisa, amd_radeon_rx_6900_xt, 4.6).
-user_rating(lisa, nvidia_geforce_rtx_3080, 4.9).
 user_rating(carl, corsair_vengeance_rgb_pro_16gb, 4.0).
-user_rating(carl, g_skill_trident_z_neo_32gb, 4.2).
-user_rating(carl, kingston_hyperx_predator_8gb, 3.8).
-user_rating(den, intel_i9_10900k, 4.2).
-user_rating(den, nvidia_geforce_rtx_3090, 4.5).
-user_rating(den, corsair_vengeance_rgb_pro_16gb, 4.1).
-user_rating(abb, nvidia_geforce_rtx_3090, 4.7).
-user_rating(abb, intel_i9_10900k, 4.6).
-user_rating(abb, amd_radeon_rx_6900_xt, 4.5).
-user_rating(don, nvidia_geforce_rtx_3090, 4.9).
 user_rating(don, intel_i9_10900k, 4.8).
+user_rating(abb, nvidia_geforce_rtx_3090, 4.7).
+user_rating(den, intel_i9_10900k, 4.2).
+user_rating(john, amd_ryzen_9_5950x, 4.8).
+user_rating(lisa, amd_radeon_rx_6900_xt, 4.6).
+user_rating(carl, g_skill_trident_z_neo_32gb, 4.2).
+user_rating(don, nvidia_geforce_rtx_3090, 4.9).
+user_rating(abb, intel_i9_10900k, 4.6).
+user_rating(den, nvidia_geforce_rtx_3090, 4.5).
+user_rating(john, intel_core_i7_11700k, 4.4).
+user_rating(lisa, nvidia_geforce_rtx_3080, 4.9).
+user_rating(carl, kingston_hyperx_predator_8gb, 3.8).
 user_rating(don, amd_ryzen_7_5800x, 4.7).
+user_rating(abb, amd_radeon_rx_6900_xt, 4.5).
+user_rating(den, corsair_vengeance_rgb_pro_16gb, 4.1).
 
-% Rule to calculate similarity between two components based on performance
-similarity(Component1, Component2, Similarity) :-
-    performance(Component1, Perf1),
-    performance(Component2, Perf2),
-    Similarity is 1 / (1 + abs(Perf1 - Perf2)).
 
-% Rule to recommend components to a user based on their search history
-recommendations(User, RecommendedComponents) :-
+% Rule to recommend products to a user
+recommend(User, RecommendedProducts) :-
+    user(User),
+    findall(Product, recommend_product(User, Product), RecommendedProducts).
+
+% Rule to recommend a product to a user based on their search history and preferences
+recommend_product(User, Product) :-
+    user(User),
     search_history(User, SearchHistory),
-    findall(Component, (
-        category(Component, Category),
-        member(Component, SearchHistory),
-        not(user_rating(User, Component, _))
-    ), UnratedComponents),
-    findall(Component-Similarity, (
-        member(Component, UnratedComponents),
-        similarity(Component, PreviousComponent, Similarity),
-        user_rating(User, PreviousComponent, _)
-    ), ComponentSimilarities),
-    sort(2, @>=, ComponentSimilarities, SortedComponentSimilarities),
-    remove_duplicates(SortedComponentSimilarities, UniqueComponentSimilarities),
-    maplist(get_component, UniqueComponentSimilarities, TempRecommendedComponents),
-    remove_duplicates(TempRecommendedComponents, RecommendedComponents).
+    preferred_category(User, PreferredCategory),
+    category(Product, PreferredCategory),
+    \+ member(Product, SearchHistory),
+    \+ already_rated(User, Product).
 
-% Rule to remove duplicates from a list
-remove_duplicates([], []).
-remove_duplicates([X|Xs], [X|Ys]) :-
-    subtract(Xs, [X], Temp),
-    remove_duplicates(Temp, Ys).
+% Rule to determine preferred category for a user
+preferred_category(User, PreferredCategory) :-
+    user(User),
+    search_history(User, SearchHistory),
+    preferred_category_helper(SearchHistory, PreferredCategory).
 
-% Rule to get the component from a tuple (Component-Similarity)
-get_component(Component-_, Component).
+% Helper rule to find preferred category based on search history
+preferred_category_helper(SearchHistory, PreferredCategory) :-
+    count_categories(SearchHistory, CategoryCounts),
+    max_category(CategoryCounts, PreferredCategory).
 
-% Sample query to get recommendations for user "john"
-% recommendations(john, RecommendedComponents).
+% Rule to count the occurrences of each category in a list of products
+count_categories([], []).
+count_categories([Product | Rest], [Category-Count | CategoryCountsRest]) :-
+    category(Product, Category),
+    count_category(Category, Rest, RestCount, Count),
+    count_categories(RestCount, CategoryCountsRest).
 
+% Helper rule to count occurrences of a specific category
+count_category(_, [], [], 0).
+count_category(Category, [Product | Rest], RestCount, Count) :-
+    category(Product, Category),
+    count_category(Category, Rest, RestCount, RestCountCount),
+    Count is RestCountCount + 1.
+count_category(Category, [Product | Rest], [Product | RestCount], Count) :-
+    category(Product, DifferentCategory),
+    DifferentCategory \= Category,
+    count_category(Category, Rest, RestCount, Count).
+
+% Rule to find the category with the maximum count
+max_category([Category-Count | Rest], MaxCategory) :-
+    max_category_helper(Rest, Category, Count, MaxCategory).
+
+% Helper rule to find the category with the maximum count
+max_category_helper([], MaxCategory, _, MaxCategory).
+max_category_helper([Category-Count | Rest], _, MaxCount, MaxCategory) :-
+    Count > MaxCount,
+    max_category_helper(Rest, Category, Count, MaxCategory).
+max_category_helper([_ | Rest], PrevCategory, MaxCount, MaxCategory) :-
+    max_category_helper(Rest, PrevCategory, MaxCount, MaxCategory).
+
+% Rule to check if a user has already rated a product
+already_rated(User, Product) :-
+    user(User),
+    user_rating(User, Product, _).
